ffca8483412caec12e13c6c2db1db336ee33d174
Dr Nic Williams <drnicwilliams@gmail.com>
2018-05-23 18:52:52 +1000

    not using an icon for text

--- public/gaf2/js/main.js
+++ public/gaf2/js/main.js
@@ -78,9 +78,7 @@ $(function () {
     });
 
     areaCenter = turf.center(areaGeoJSON, 1);
-    areaCenter.properties = {
-      "title": id
-    }
+    areaCenter.properties = {"title": id}
     map.addLayer({
       "id": "label-" + id,
       "type": "symbol",
@@ -89,10 +87,8 @@ $(function () {
           "data": areaCenter
       },
       "layout": {
-          "icon-image": "{icon}-15",
           "text-field": "{title}",
           "text-font": ["Open Sans Semibold", "Arial Unicode MS Bold"],
-          "text-offset": [0, 0.6],
           "text-anchor": "top"
       }
     });
