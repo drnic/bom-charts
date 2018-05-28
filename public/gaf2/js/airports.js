62ab76d94660acb4003c65892e8560562ad850c6
Dr Nic Williams <drnicwilliams@gmail.com>
2018-05-29 08:20:57 +1000

    move airport loading into own file

---
+++ public/gaf2/js/airports.js
@@ -0,0 +1,41 @@
+$(function () {
+    map.on('load', function () {
+    $.get("/json/openflights-airports-au.json", function (data) {
+      console.log(data[0]);
+      var airportsCollection = data.reduce(function(result, airport) {
+        var feature = {
+          "type": "Feature",
+          "geometry": {
+              "type": "Point",
+              "coordinates": [airport["Longitude"], airport["Latitude"]]
+          },
+          "properties": {
+              "title": airport["ICAO"]
+          }
+        };
+        result.push(feature);
+        return result;
+      }, []);
+
+      var airportsGeoJSON = {
+        "type": "geojson",
+        "data": {
+          "type": "FeatureCollection",
+          "features": airportsCollection
+        }
+      };
+
+      map.addLayer({
+        "id": "airports",
+        "type": "symbol",
+        "source": airportsGeoJSON,
+        "layout": {
+          "text-field": "{title}",
+          "text-font": ["Open Sans Semibold", "Arial Unicode MS Bold"],
+          "text-offset": [0, 0],
+          "text-anchor": "top"
+        }
+      });
+    });
+  });
+});
--- public/gaf2/js/main.js
+++ public/gaf2/js/main.js
@@ -162,44 +162,6 @@ $(function () {
       });
     });
 
-    $.get("/json/openflights-airports-au.json", function (data) {
-      console.log(data[0]);
-      var airportsCollection = data.reduce(function(result, airport) {
-        var feature = {
-          "type": "Feature",
-          "geometry": {
-              "type": "Point",
-              "coordinates": [airport["Longitude"], airport["Latitude"]]
-          },
-          "properties": {
-              "title": airport["ICAO"]
-          }
-        };
-        result.push(feature);
-        return result;
-      }, []);
-
-      var airportsGeoJSON = {
-        "type": "geojson",
-        "data": {
-          "type": "FeatureCollection",
-          "features": airportsCollection
-        }
-      };
-
-      map.addLayer({
-        "id": "airports",
-        "type": "symbol",
-        "source": airportsGeoJSON,
-        "layout": {
-          "text-field": "{title}",
-          "text-font": ["Open Sans Semibold", "Arial Unicode MS Bold"],
-          "text-offset": [0, 0],
-          "text-anchor": "top"
-        }
-      });
-    });
-
     $.get("/json/lsalt-au.json", function (data) {
       data.forEach(lsaltGrid => {
         var grid = lsaltGrid["grid"]
