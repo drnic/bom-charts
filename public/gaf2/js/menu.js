0058b3c1679e569429a6e7c5e2a0bce9e7b20108
Dr Nic Williams <drnicwilliams@gmail.com>
2018-06-06 15:41:41 +1000

    don't add link to show which vfr/period is currently selected

--- public/gaf2/js/menu.js
+++ public/gaf2/js/menu.js
@@ -3,8 +3,15 @@ $(function () {
   var period = getUrlParameter("period") || "current";
 
   var menu = $("p#menu")
-  menu.find("a#day-vfr").attr("href", `?vfr=day&period=${period}`);
-  menu.find("a#night-vfr").attr("href", `?vfr=night&period=${period}`);
-  menu.find("a#period-current").attr("href", `?vfr=${vfr}&period=current`);
-  menu.find("a#period-next").attr("href", `?vfr=${vfr}&period=next`);
+  if (vfr == "night") {
+    menu.find("a#day-vfr").attr("href", `?vfr=day&period=${period}`);
+  } else {
+    menu.find("a#night-vfr").attr("href", `?vfr=night&period=${period}`);
+  }
+
+  if (period == "next") {
+    menu.find("a#period-current").attr("href", `?vfr=${vfr}&period=current`);
+  } else {
+    menu.find("a#period-next").attr("href", `?vfr=${vfr}&period=next`);
+  }
 });
