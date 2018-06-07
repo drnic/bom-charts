6056a732bc34e2d0a6e643fb34de6f2530af633e
Dr Nic Williams <drnicwilliams@gmail.com>
2018-06-07 15:32:19 +1000

    don't need to load all lsalt grids at the moment; until refreshing map

--- public/gaf2/js/gafdata.js
+++ public/gaf2/js/gafdata.js
@@ -13,21 +13,32 @@ if (period == "next") {
 
 $(document).ready(function () {
   map.on("load", function() {
-    gafVFROptions.forEach(_vfr => {
-      gafData[_vfr] = {}
-
-      gafPeriods.forEach(_period => {
-        gafData[_vfr][_period] = {}
-
-        gafAreaCodes.forEach(gafAreaCode => {
-          $.getJSON(`/api/gafarea/${gafAreaCode}/${_period}/${_vfr}.json`, function(data) {
-            gafData[_vfr][_period][gafAreaCode] = data;
-            if (_vfr == vfr && _period == period) {
-                  setupMapForGAFArea(gafAreaCode, data);
-                }
-            });
+    gafData[vfr] = {}
+    gafData[vfr][period] = {}
+    gafAreaCodes.forEach(gafAreaCode => {
+      $.getJSON(`/api/gafarea/${gafAreaCode}/${period}/${vfr}.json`, function(data) {
+        gafData[vfr][period][gafAreaCode] = data;
+          setupMapForGAFArea(gafAreaCode, data);
         });
-      });
     });
   });
+  // For future:
+  // map.on("load", function() {
+  //   gafVFROptions.forEach(_vfr => {
+  //     gafData[_vfr] = {}
+
+  //     gafPeriods.forEach(_period => {
+  //       gafData[_vfr][_period] = {}
+
+  //       gafAreaCodes.forEach(gafAreaCode => {
+  //         $.getJSON(`/api/gafarea/${gafAreaCode}/${_period}/${_vfr}.json`, function(data) {
+  //           gafData[_vfr][_period][gafAreaCode] = data;
+  //           if (_vfr == vfr && _period == period) {
+  //                 setupMapForGAFArea(gafAreaCode, data);
+  //               }
+  //           });
+  //       });
+  //     });
+  //   });
+  // });
 });
