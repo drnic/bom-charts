737be553a529f0dabe724ea8dbec0429fa4c3a40
Dr Nic Williams <drnicwilliams@gmail.com>
2018-06-07 14:06:15 +1000

    can load airport names earlier before map loads

--- public/gaf2/js/airports.js
+++ public/gaf2/js/airports.js
@@ -1,29 +1,29 @@
-$(function () {
-  map.on('load', function () {
-    $.get("/json/openflights-airports-au.json", function (data) {
-      var airportsCollection = data.reduce((result, airport) => {
-        var feature = {
-          "type": "Feature",
-          "geometry": {
-              "type": "Point",
-              "coordinates": [airport["Longitude"], airport["Latitude"]]
-          },
-          "properties": {
-              "title": airport["ICAO"]
-          }
-        };
-        result.push(feature);
-        return result;
-      }, []);
+$.get("/json/openflights-airports-au.json", function (data) {
+  var airportsCollection = data.reduce((result, airport) => {
+    var feature = {
+      "type": "Feature",
+      "geometry": {
+          "type": "Point",
+          "coordinates": [airport["Longitude"], airport["Latitude"]]
+      },
+      "properties": {
+          "title": airport["ICAO"]
+      }
+    };
+    result.push(feature);
+    return result;
+  }, []);
 
-      var airportsGeoJSON = {
-        "type": "geojson",
-        "data": {
-          "type": "FeatureCollection",
-          "features": airportsCollection
-        }
-      };
+  var airportsGeoJSON = {
+    "type": "geojson",
+    "data": {
+      "type": "FeatureCollection",
+      "features": airportsCollection
+    }
+  };
 
+  $(function () {
+    map.on('load', function () {
       map.addLayer({
         "id": "airports",
         "type": "symbol",
