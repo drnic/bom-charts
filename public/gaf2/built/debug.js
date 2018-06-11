6d2cf47d557c4d0caa604386634b447e32425d47
Dr Nic Williams <drnicwilliams@gmail.com>
2018-06-11 14:05:02 +1000

    remove built/ artifacts from future repo changes

--- public/gaf2/built/debug.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const mapui = require("./debug/mapui-debug");
-function init() {
-    mapui.init();
-}
-exports.init = init;
--- public/gaf2/built/debug/mapui.js
+++
@@ -1,9 +0,0 @@
-import * as $ from 'jquery';
-import * as wait from "../helpers/wait";
-export function init() {
-    let styleloaded = $('<span/>');
-    styleloaded.text(`-`);
-    styleloaded.appendTo($('body'));
-    wait.runEvery(100, () => {
-    });
-}
