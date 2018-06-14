4a868d352345bf5f43313cba15b32368dced47f2
Dr Nic Williams <drnicwilliams@gmail.com>
2018-06-14 19:21:56 +1000

    use a remote FeatureCollection and a single Layer for LSALT grids

--- public/gaf2/src/lsalt.ts
+++ public/gaf2/src/lsalt.ts
@@ -8,101 +8,61 @@ import * as random from "./helpers/random";
 import * as wait from "./helpers/wait";
 import * as turf from "@turf/helpers";
 import * as turfintersect from "@turf/intersect";
+import { GeoJSONSource } from 'mapbox-gl';
 
 export interface LSALTGrid {
   grid: number[][];
   lsalt_100ft: number;
 }
 
-let lsaltData: { [gafAreaCode: string]: LSALTGrid[] } = {}
+let map = mapui.map;
 
 export function init() {
   update();
   wait.runEvery(1000 * 60 * 60, update);
+  // wait.runEvery(1000, update);
 }
 
 function update() {
   let vfr = controller.vfr;
+  let url = `/lsalt?vfr=${vfr}`;
+
+  setupLayer();
+
   $.getJSON(`/lsalt?vfr=${vfr}`, (data) => {
-    console.log(data);
+    let sourceID = `lsalt-${vfr}`;
+    let source = <GeoJSONSource>map.getSource(sourceID);
+    source.setData(data);
+    console.log("update lsalt")
   });
 }
 
-var layerIDsForLSALT : { [gafAreaCode: string]: string[] } = {};
-
-// TODO: could this be done once on the server for each GAF area/period?
-// Perhaps server needs rewriting in nodejs so can use turfjs.intersect?
-// Or perhaps golang calls out to nodejs CLIs to carve up XML + LSALT and cache on disk
-// Schedule task that fetches XML -> S3 could also perform the carving with LSALT and store to S3
-// Golang app can then fetch XML + carve up from S3 and cache in memory; rather than from BOM.
-function updateLSALT(gafAreaCode: string) {
-  return;
-  let map = mapui.map;
-  let nightVFR = controller.isNightVFR();
-
-  var mapAreas = gafarea.mapAreasByAreaCode[gafAreaCode];
-  var lsaltGridsForArea = lsaltData[gafAreaCode];
-  if (mapAreas === undefined || mapAreas.length === 0 ||
-    lsaltGridsForArea === undefined || lsaltGridsForArea.length === 0) {
-    return;
-  }
-  if (layerIDsForLSALT[gafAreaCode]) {
-    return;
-  }
-  layerIDsForLSALT[gafAreaCode] = [];
-
-  lsaltGridsForArea.forEach(lsaltGrid => {
-    wait.delay(1).then(() => {
-      var grid = lsaltGrid.grid;
-      var lsalt = lsaltGrid.lsalt_100ft;
-      if (!nightVFR) {
-        // assume pilot can see highest object; and that LSALT is 1300' higher than highest object
-        lsalt -= 13;
-      }
-
-      var lsaltPolygon = turf.polygon([grid]);
-
-      mapAreas.forEach(mapArea => {
-        var mapAreaPolygon = mapArea.turfPolygon();
-
-        var lsaltIntersection = turfintersect.default(mapAreaPolygon, lsaltPolygon)
-        if (lsaltIntersection === undefined) {
-          return;
-        }
-
-        var layerID = "lsalt" + random.UUID();
-        layerIDsForLSALT[gafAreaCode].push(layerID);
-        var areaCloudLayerBase = mapArea.cloudBase() === undefined ? 10000 : mapArea.cloudBase();
-        var cloudBaseLSALTDelta = areaCloudLayerBase - (lsalt * 100);
-        var layerColourIndex = Math.round(cloudBaseLSALTDelta / 1000);
-        layerColourIndex = Math.min(3, Math.max(0, layerColourIndex));
-        var layerColour = theme.current().cssHeightColors[layerColourIndex];
-
-        if (mapArea.cloudBaseColor() != layerColour) {
-          map.addLayer({
-            "id": layerID,
-            "type": "fill",
-            "source": {
-              "type": "geojson", "data": lsaltIntersection
-            },
-            "paint": {
-              "fill-color": layerColour,
-              "fill-antialias": false,
-              "fill-opacity": 0.5
-            }
-          });
-
-          map.on("mouseover", layerID, () => {
-            map.setPaintProperty(layerID, "fill-opacity", 0.8);
-            var text = "LSALT: " + lsaltGrid.lsalt_100ft;
-            $('#mouseover-summary-lsalt-grid').text(text);
-          });
-
-          map.on("mouseleave", layerID, () => {
-            map.setPaintProperty(layerID, "fill-opacity", 0.5);
-          });
-        }
-      });
-    })
+function setupLayer() {
+  map.addSource(`lsalt-day`, {
+    type: "geojson",
+    data: {
+      type: "FeatureCollection",
+      features: []
+    }
   });
-};
+
+  map.addSource(`lsalt-night`, {
+    type: "geojson",
+    data: {
+      type: "FeatureCollection",
+      features: []
+    }
+  });
+
+  let vfr = controller.vfr;
+  map.addLayer({
+    "id": `lsalt`,
+    "type": "fill",
+    "source": `lsalt-${vfr}`,
+    "paint": {
+      "fill-color": '#050',
+      "fill-antialias": false,
+      "fill-opacity": 0.5
+    }
+  });
+}
